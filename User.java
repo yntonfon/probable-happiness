25fda8a498ed656975763c5ff5aaf72cc29646c6
yntonfon <ynguieye@gmail.com>
2016-10-28 14:20:39 +0200

    Add new User class

---
+++ User.java
@@ -0,0 +1,9 @@
+public final class User {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String sexe;
+    private float weight;
+
+    public User () {}
+}
