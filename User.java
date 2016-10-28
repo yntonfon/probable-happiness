3d04f91973ed4540a138a815ae35335bb5b79c6f
yntonfon <ynguieye@gmail.com>
2016-10-28 18:25:53 +0200

    Add s to heights attribute

--- User.java
+++ User.java
@@ -4,7 +4,7 @@ public final class User {
     private Date birthDate;
     private String sexe;
     private float weight;
-    private float height;
+    private float heights;
 
     public User () {}
 }
